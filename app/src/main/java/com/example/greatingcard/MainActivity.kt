9901e4cf6c93ae55ca8f17a2a083adb036b5135f
faizGhazali <ahmadfaiz21@outlook.com>
2024-04-03 12:15:29 +0800

    Discover Android Studio Kotlin Basic
    Discover the useage of :
    @Composable
    @Preview
    surrond with widget
    
    of the android studio :+1:

--- app/src/main/java/com/example/greatingcard/MainActivity.kt
+++ app/src/main/java/com/example/greatingcard/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.greatingcard.ui.theme.GreatingCardTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +26,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting("Faiz")//set layout from jetpack @compose
                 }
             }
         }
@@ -31,16 +35,20 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Surface(color=Color.Cyan) { //surround with widget ctlr+alt+t
+        Text(
+            text = "Hi, my name is $name!",
+            modifier = modifier.padding(24.dp)
+
+        )
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     GreatingCardTheme {
-        Greeting("Android")
+        Greeting("Faiz")
     }
 }
