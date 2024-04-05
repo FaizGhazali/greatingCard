081d2fc7e7ff52815b6cdf9def12050d5646240b
Faiz Ghazali <99084931+FaizGhazali@users.noreply.github.com>
2024-04-06 05:19:21 +0800

    figure out layout

--- app/src/main/java/com/example/greatingcard/MainActivity.kt
+++ app/src/main/java/com/example/greatingcard/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.greatingcard
 
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Email
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -24,6 +25,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -37,8 +39,8 @@ class MainActivity : ComponentActivity() {
             GreatingCardTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                        modifier = Modifier.fillMaxSize(),
-                        color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
                 ) {
                     //Preview
                     ComposeQuadApp()
@@ -55,73 +57,149 @@ fun ComposeQuadApp() {
         Row(Modifier.weight(1f)) {
 
             //2 item
-            ComposableInfoCard(title = "Text composable",
-                    description = "Displays text and follows the recommended Material Design guidelines.",
-                    bgColor = Color(0xFFEADDFF),
-                    modifier = Modifier.weight(1f)
+            ComposableInfoCard(
+                title = "Text composable",
+                description = "Displays text and follows the recommended Material Design guidelines.",
+                bgColor = Color(0xFFEADDFF),
+                modifier = Modifier.weight(1f)
             )
-            ComposableInfoCard(title = "Image composable",
-                    description = "Creates a composable that lays out and draws a given Painter class object.",
-                    bgColor = Color(0xFFD0BCFF),
-                    modifier = Modifier.weight(1f)
+            ComposableInfoCard(
+                title = "Image composable",
+                description = "Creates a composable that lays out and draws a given Painter class object.",
+                bgColor = Color(0xFFD0BCFF),
+                modifier = Modifier.weight(1f)
             )
-            
+
 
         }
         Row(Modifier.weight(1f)) {
             //2item
-            ComposableInfoCard(title = "Row composable",
-                    description = "A layout composable that places its children in a horizontal sequence.",
-                    bgColor = Color(0xFFB69DF8),
-                    modifier = Modifier.weight(1f)
+            ComposableInfoCard(
+                title = "Row composable",
+                description = "A layout composable that places its children in a horizontal sequence.",
+                bgColor = Color(0xFFB69DF8),
+                modifier = Modifier.weight(1f)
             )
-            ComposableInfoCard(title = "Column composable",
-                    description = "A layout composable that places its children in a vertical sequence.",
-                    bgColor = Color(0xFFF6EDFF),
-                    modifier = Modifier.weight(1f)
+            ComposableInfoCard(
+                title = "Column composable",
+                description = "A layout composable that places its children in a vertical sequence.",
+                bgColor = Color(0xFFF6EDFF),
+                modifier = Modifier.weight(1f)
             )
         }
 
     }
 
+
+}
+
+@Composable
+fun ComposeBusinessCardApp() {
+    Column(Modifier.fillMaxWidth()) {
+        Row() {
+            ComposableProfile(
+                profilePicture = painterResource(R.drawable.ppicture),
+                name = "Faiz Ghazali", title = "Software Developer",
+                phoneNumber = "+601-2022352",
+                socialMedia = "faizGhazra21",
+                email = "kisame1990@gmail.com"
+            )
+        }
+    }
+}
+
+@Composable
+private fun ComposableProfile(
+    profilePicture: Painter,
+    name: String,
+    title: String,
+    phoneNumber: String,
+    socialMedia: String,
+    email: String,
+    modifier:
+    Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize(),
+
+
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row {
+            Image(painter = profilePicture, contentDescription = null) // how to do this
+        }
+        Row {
+            Text(
+                text = name,
+
+                )
+        }
+        Row {
+            Text(
+                text = title,
+
+                )
+        }
+    }
+    Column(
+        modifier = modifier.fillMaxWidth(),
+
+        verticalArrangement = Arrangement.Bottom,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = phoneNumber,
+
+            )
+        Text(
+            text = socialMedia,
+
+            )
+        Text(
+            text = email,
+
+            )
+    }
 
 }
 
 @Composable
 private fun ComposableInfoCard(
-        title: String,
-        description: String,
-        bgColor: Color,
-        modifier: Modifier = Modifier,
+    title: String,
+    description: String,
+    bgColor: Color,
+    modifier: Modifier = Modifier,
 ) {
     Column(
-            modifier = modifier
-                    .fillMaxSize()
-                    .background(bgColor),
+        modifier = modifier
+            .fillMaxSize()
+            .background(bgColor),
 
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
-                text = title,
-                modifier = Modifier.padding(bottom = 16.dp),
-                fontWeight = FontWeight.Bold
+            text = title,
+            modifier = Modifier.padding(bottom = 16.dp),
+            fontWeight = FontWeight.Bold
         )
         Text(
-                text = description,
-                modifier = Modifier.padding(start = 16.dp,end = 16.dp),
-                textAlign = TextAlign.Justify
+            text = description,
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+            textAlign = TextAlign.Justify
         )
     }
 
 }
 
 
-@Preview(showBackground = true,)
+@Preview(showBackground = true)
 @Composable
 fun QuadrantPreview() {
     GreatingCardTheme {
-        ComposeQuadApp()
+        ComposeBusinessCardApp()
     }
 }
 
