ce71b6db1f69c80378a62e393dc44e7b85b6ec10
faizGhazali <ahmadfaiz21@outlook.com>
2024-04-17 15:30:41 +0800

    v8- Roll Dice
    -Button Clicked, will randomize dice

--- app/src/main/java/com/example/greatingcard/MainActivity.kt
+++ app/src/main/java/com/example/greatingcard/MainActivity.kt
@@ -27,8 +27,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.modifier.modifierLocalConsumer
@@ -37,7 +39,12 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Menu
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 
 class MainActivity : ComponentActivity() {
@@ -45,315 +52,44 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             GreatingCardTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    //Preview
-                    ComposeQuadApp()
-                }
+                DiceRollerApp()
             }
         }
     }
 }
-
-
 @Composable
-fun ComposeQuadApp() {
-    Column(Modifier.fillMaxWidth()) { //max item width
-        Row(Modifier.weight(1f)) {
+fun DiceWithBUttonAndImage(modifier: Modifier = Modifier){
 
-            //2 item
-            ComposableInfoCard(
-                title = "Text composable",
-                description = "Displays text and follows the recommended Material Design guidelines.",
-                bgColor = Color(0xFFEADDFF),
-                modifier = Modifier.weight(1f)
-            )
-            ComposableInfoCard(
-                title = "Image composable",
-                description = "Creates a composable that lays out and draws a given Painter class object.",
-                bgColor = Color(0xFFD0BCFF),
-                modifier = Modifier.weight(1f)
-            )
-
-
-        }
-        Row(Modifier.weight(1f)) {
-            //2item
-            ComposableInfoCard(
-                title = "Row composable",
-                description = "A layout composable that places its children in a horizontal sequence.",
-                bgColor = Color(0xFFB69DF8),
-                modifier = Modifier.weight(1f)
-            )
-            ComposableInfoCard(
-                title = "Column composable",
-                description = "A layout composable that places its children in a vertical sequence.",
-                bgColor = Color(0xFFF6EDFF),
-                modifier = Modifier.weight(1f)
-            )
-        }
-
+    var result by remember { mutableStateOf(1)}
+    val imageResource = when (result){
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
     }
-
-
-}
-
-@Composable
-fun ComposeBusinessCardApp() {
-
-
-        Box(
-
-            contentAlignment = Alignment.Center,
-
-        ) {
-            Image(
-                painter = painterResource(R.drawable.androidparty),
-                contentDescription = null,
-                contentScale = ContentScale.Crop,
-                modifier = Modifier.fillMaxSize()
-            )
-            Column(
-                Modifier
-                    .fillMaxWidth()
-                    .padding(20.dp)
-                    .border(2.dp, Color.Red),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-
-            ) {
-                Column(
-                    Modifier
-                        .fillMaxWidth()
-                        .padding(start = 4.dp, end = 4.dp, top = 4.dp)
-                        .border(2.dp, Color.Blue),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Image(
-                        painter = painterResource(R.drawable.ppicture),
-                        contentDescription = null,
-                        modifier = Modifier.size(62.dp)
-                    )
-                }
-                Column(
-                    Modifier
-                        .fillMaxWidth()
-                        .padding(start = 4.dp, end = 4.dp, top = 2.dp)
-                        .border(2.dp, Color.Blue),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Text(text = "Ahmad Faiz bin Ghazali")
-                }
-                Column(
-                    Modifier
-                        .fillMaxWidth()
-                        .padding(start = 4.dp, end = 4.dp, top = 2.dp, bottom = 4.dp)
-                        .border(2.dp, Color.Blue),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Text(text = "Programmer")
-                }
-            }
-        }
-        Box(
-            Modifier.fillMaxSize(),
-            contentAlignment = Alignment.BottomCenter
-
-        ) {
-
-            Column(
-                Modifier
-                    .fillMaxWidth()
-                    .padding(start = 20.dp, end = 20.dp, top = 0.dp, bottom = 40.dp)
-                    .border(2.dp, Color.Red),
-                verticalArrangement = Arrangement.Center, //left justified
-                horizontalAlignment = Alignment.CenterHorizontally
-
-            ) {
-                Column(
-                    Modifier
-                        .fillMaxWidth()
-                        .padding(start = 4.dp, end = 4.dp, top = 4.dp)
-                        .border(2.dp, Color.Blue),
-
-                    horizontalAlignment = Alignment.Start
-                ) {
-                    Row(
-                        Modifier.padding
-                            (start = 50.dp, top = 4.dp, bottom = 4.dp)
-                    ) {
-                        Icon(
-                            painter = painterResource(R.drawable.social_media),
-                            contentDescription = null
-                        )
-                        Spacer(modifier = Modifier.width(10.dp))
-                        Text(
-                            text = "+60-132022352",
-                            modifier = Modifier.border(2.dp, Color.Magenta)
-                        )
-                        //Space 10dp
-                    }
-                }
-                Column(
-                    Modifier
-                        .fillMaxWidth()
-                        .padding(start = 4.dp, end = 4.dp, top = 2.dp)
-                        .border(2.dp, Color.Blue),
-
-                    horizontalAlignment = Alignment.Start
-                ) {
-                    Row(
-                        Modifier.padding
-                            (start = 50.dp, top = 4.dp, bottom = 4.dp)
-                    ) {
-                        Icon(
-                            painter = painterResource(R.drawable.contact),
-                            contentDescription = null
-                        )
-                        Spacer(modifier = Modifier.width(10.dp))
-                        Text(text = "@faizghazra", modifier = Modifier.border(2.dp, Color.Magenta))
-                    }
-                }
-                Column(
-                    Modifier
-                        .fillMaxWidth()
-                        .padding(start = 4.dp, end = 4.dp, top = 2.dp, bottom = 4.dp)
-                        .border(2.dp, Color.Blue),
-
-
-                    horizontalAlignment = Alignment.Start
-                ) {
-                    Row(
-                        Modifier.padding
-                            (start = 50.dp, top = 4.dp, bottom = 4.dp)
-                    ) {
-                        Icon(painter = painterResource(R.drawable.email), contentDescription = null)
-                        Spacer(modifier = Modifier.width(10.dp))
-                        Text(
-                            text = "ahmadfaiz21@outlook.com",
-                            modifier = Modifier.border(2.dp, Color.Magenta)
-                        )
-                    }
-
-                }
-            }
-        }
-
-}
-
-@Composable
-private fun ComposableProfile(
-    profilePicture: Painter,
-    name: String,
-    title: String,
-    phoneNumber: String,
-    socialMedia: String,
-    email: String,
-    modifier:
-    Modifier = Modifier
-) {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
-    ) {
-        Column(
-            modifier = Modifier.align(Alignment.Center),
-            verticalArrangement = Arrangement.Center
-        ) {
-            Box(
-                modifier = Modifier
-                    .size(100.dp)
-
-            ) {
-                Row {
-                    Image(painter = profilePicture, contentDescription = null) // how to do this
-                }
-
-            }
-            Box {
-                Row {
-                    Text(
-                        text = name,
-
-                        )
-                }
-            }
-            Row {
-                Text(
-                    text = title,
-
-                    )
-            }
-        }
-        Column(
-            modifier = Modifier.align(Alignment.BottomCenter),
-
-
-            ) {
-            Text(
-                text = phoneNumber, textAlign = TextAlign.Center
-
-
-            )
-            Text(
-                text = socialMedia,
-                textAlign = TextAlign.Center
-
-            )
-            Text(
-                text = email,
-
-                )
-        }
-
-    }
-
-
-}
-
-@Composable
-private fun ComposableInfoCard(
-    title: String,
-    description: String,
-    bgColor: Color,
-    modifier: Modifier = Modifier,
-) {
-    Column(
-        modifier = modifier
-            .fillMaxSize()
-            .background(bgColor),
-
-        verticalArrangement = Arrangement.Center,
+    Column (
+        modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = title,
-            modifier = Modifier.padding(bottom = 16.dp),
-            fontWeight = FontWeight.Bold
+    ){
+        Image(
+            painter = painterResource(imageResource),
+            contentDescription = "1"
         )
-        Text(
-            text = description,
-            modifier = Modifier.padding(start = 16.dp, end = 16.dp),
-            textAlign = TextAlign.Justify
-        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { result = (1..6).random() }) {
+            Text(stringResource(R.string.roll))
+
+        }
     }
-
 }
-
-
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun QuadrantPreview() {
-    GreatingCardTheme {
-        ComposeBusinessCardApp()
-    }
+fun DiceRollerApp(){
+    DiceWithBUttonAndImage(modifier = Modifier
+        .fillMaxSize()
+        .wrapContentSize(Alignment.Center)
+    )
 }
-
 
